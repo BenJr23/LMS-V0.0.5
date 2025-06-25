3dd7f49878655c011feece4926869b1546dce83b
BenJr23 <rubenjrtbertuso@gmail.com>
2025-06-25 19:51:24 +0800

    renew db

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'gdhdyplrwcnrcfxeiiwp.supabase.co',
+        hostname: 'nxxjoelvjlhmgwmqjsjg.supabase.co',
         port: '',
         pathname: '/storage/v1/object/public/lms/**',
       },
--- prisma/migrations/20250617074719_edited_type_shi/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Submission" ALTER COLUMN "content" DROP NOT NULL,
-ALTER COLUMN "filePath" DROP NOT NULL;
